905dec1d7830958cd88a9d83b47605b657480e5f
giammy00 <puleogianm@gmail.com>
2022-07-24 10:00:47 +0200

    corretto file inquinamento

--- src/trentodatalib/inquinamento.py
+++ src/trentodatalib/inquinamento.py
@@ -3,7 +3,7 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 try:
     dfInqTrento = pd.read_pickle( os.path.join(os.path.dirname(__file__),"../../data/interim/datiInquinamento.pkl"))
-    df_inquinamento = pd.read_pickle(  os.path.join(os.path.dirname(__file__),"../../data/interim/datitotinquinamento.pkl") )
+    df_inquinamento = pd.read_pickle(  os.path.join(os.path.dirname(__file__),"../../data/interim/datiTotInquinamento.pkl") )
 except FileNotFoundError:
     print("Importazione dei file inquinamento non riuscita. Assicurarsi di aver creato i dataset dell'inquinamento usando make_dataset_inquinamento.py")
     
